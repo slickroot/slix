54164a70f19ca45fc1c1e6f1ed50b06623f19ebb
Marouane Elaich <elaich@gmx.net>
2026-09-21 15:55:59 +0800

    bootstrap slix crate: nix flake dev shell, serde deps

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "slix development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [ cargo rustc rustfmt ];
+          };
+        });
+    };
+}
